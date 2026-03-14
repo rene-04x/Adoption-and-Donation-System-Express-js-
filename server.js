69efa1167e88b40dd1694d0397ea6973af706e82
RhysandTheHL <marihernandez@my.cspc.edu.ph>
2026-03-14 23:52:09 +0800

    IMONG MAMA

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -8,11 +8,27 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/landing_page/index.html');
 });
-
-app.get('/donate', (req, res) => {
-    res.sendFile(__dirname + '/public/landing_page/donate.html');
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/public/user/home.html');
+});
+app.get('/profile', (req, res) => {
+    // Ensure the file is actually at public/user/profile.html
+    res.sendFile(path.join(__dirname, 'public', 'user', 'profile.html'));
 });
 
+app.get('/donations', (req, res) => {
+    res.sendFile(__dirname + '/public/user/donations.html');
+});
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile(__dirname + '/public/user/dashboard.html');
+});
+app.get('/adoption_hub', (req, res) => {
+    res.sendFile(__dirname + '/public/user/adoption_hub.html');
+});
+app.get('/sidebar', (req, res) => {
+    res.sendFile(__dirname + '/public/user/sidebar.html');
+});
 app.get('/about', (req, res) => {
     res.sendFile(__dirname + '/public/landing_page/about.html');
 });
@@ -36,8 +52,6 @@ app.get('/form2', (req, res) => {
 app.get('/form3', (req, res) => {
     res.sendFile(__dirname + '/public/login/form3.html');
 });
-
-
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000');
 });
